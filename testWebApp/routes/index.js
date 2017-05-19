50bcae176ef608f8a6f35bf039ca7392b71d0c70
chenergou <446492986@qq.com>
2017-05-19 10:06:29 +0800

    1、修改配置

--- testWebApp/routes/index.js
+++ testWebApp/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '小古田' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: '<h1>小古田</h1>',
+    users: [
+      { name: 'zhangjian', age: 22 },
+      { name: 'zhangjian', age: 22 },
+      { name: 'zhangjian', age: 23 },
+      { name: 'zhangjian', age: 24 },
+    ]
+  });
 });
 
 module.exports = router;
