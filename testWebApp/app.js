03eff4bbc61ccae8f0e660b608fd3470914fc3bc
chenergou <446492986@qq.com>
2017-05-22 18:35:04 +0800

    1、修改图标

--- testWebApp/app.js
+++ testWebApp/app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'xiaogutian.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 console.log('33',path.join(__dirname, 'public', 'xiaogutian.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
